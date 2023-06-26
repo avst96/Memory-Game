ac72c233e3eb7e2b6936d171482a8e462f037c3c
avst96 <avst96@gmail.com>
2023-06-26 15:18:41 -0500

    #3 In process of setting up computer turn

--- MemoryGameApp/MemoryGameApp/frmMemoryGame.cs
+++ MemoryGameApp/MemoryGameApp/frmMemoryGame.cs
@@ -5,6 +5,10 @@
         List<Button> allcards;
         List<List<Button>> sets = new();
         List<Button> activecards = new();
+        List<Button> pickedcards = new();
+        List<Button> availablecards = new();
+        int match1 = 0;
+        int match2 = 0;
 
         int score1 = 0, score2 = 0;
         Random rnd = new();
@@ -18,40 +22,54 @@
             allcards = new() { btnCard1, btnCard2, btnCard3, btnCard4, btnCard5, btnCard6, btnCard7, btnCard8, btnCard9, btnCard10, btnCard11, btnCard12, btnCard13, btnCard14, btnCard15, btnCard16, btnCard17, btnCard18, btnCard19, btnCard20 };
             allcards.ForEach(c => c.Click += Card_Click);
             btnStart.Click += BtnStart_Click;
+            optSolo.CheckedChanged += Opt_CheckedChanged;
         }
+
+
         private void StartNewGame()
         {
+            //both following if's can run every time the function is called without effect, however it will then do the process for no reason
+            if (gamestatus != GameStatusEnum.playing)
+            {
+                ShuffleCards();
+                currentturn = TurnEnum.player1;
+            }
+            if (gamestatus != GameStatusEnum.notstarted)
+            {
+                //To turn over all cards and put them in the deck
+                allcards.ForEach(c =>
+                {
+                    c.BackColor = Color.Orange;
+                    c.ForeColor = Color.Orange;
+                    c.Visible = true;
+                });
+            }
+
             //If pressed in middle playing will reset to not started
             gamestatus = gamestatus == GameStatusEnum.playing ? GameStatusEnum.notstarted : GameStatusEnum.playing;
 
-            currentturn = TurnEnum.player1;
             score1 = 0; score2 = 0;
-            allcards.ForEach(c =>
-            {
-                c.BackColor = Color.Orange;
-                c.ForeColor = Color.Orange;
-                c.Visible = true;
-            });
+
             SetMessageAndBtns();
-            ShuffleCards();
         }
-        private void ExposeCard(Button btn)
+        private void PlayCard(Button btn)
         {
             if (gamestatus == GameStatusEnum.playing && activecards.Count() < 2 && !activecards.Contains(btn))
             {
                 btn.BackColor = Color.LightGoldenrodYellow;
                 btn.ForeColor = Color.Black;
                 activecards.Add(btn);
+                pickedcards.Add(btn);
+
 
                 //Once 2 cards are picked, they following will proceed 
                 if (activecards.Count() == 2)
                 {
-                    ThreeSecWait();
+                    TwoSecDelay();
 
                     //To ensure that the New Game btn wasnt pressed during the wait
                     if (gamestatus == GameStatusEnum.playing)
                     {
-
 
                         //If a match
                         if (activecards[0].Text == activecards[1].Text)
@@ -75,14 +93,29 @@
                                 SetMessageAndBtns();
                             }
                         }
+
+                        //Should always run to change turn
+                        HideCard();
+
+                        if (optSolo.Checked && currentturn == TurnEnum.player2 && gamestatus == GameStatusEnum.playing)
+                        {
+                            TwoSecDelay();
+                            DoComputerMove();
+                        }
                     }
-
-                    //Should always run to change turn
-                    HideCard();
-
                 }
             }
         }
+
+        private static void TwoSecDelay()
+        {
+            DateTime starttime = DateTime.Now;
+            while ((DateTime.Now - starttime).TotalSeconds < 2)
+            {
+                Application.DoEvents();
+            }
+        }
+
         private void HideCard()
         {
             //Turns back over exposed cards, and then clears list of exposed cards and changes turn
@@ -96,16 +129,61 @@
 
             SetMessageAndBtns();
         }
-
-        private void ThreeSecWait()
+        private bool PickedCardsMatch()
         {
-            DateTime starttime = DateTime.Now;
-            while ((DateTime.Now - starttime).TotalSeconds < 3)
+            //This for should remove sets that where won from the pickedcards list
+            for (int i = pickedcards.Count - 1; i >= 0; i--)
             {
-                Application.DoEvents();
-            }
-        }
+                Button btn = pickedcards[i];
 
+                if (btn.Visible == false)
+                {
+                    pickedcards.Remove(btn);
+                }
+            }
+
+
+
+            int pickedcount = pickedcards.Count();
+
+            for (int i = 0; i < pickedcount - 1; i++)
+            {
+                for (int j = i + 1; j < pickedcount - 1; j++)
+                {
+                    match1 = i;
+                    match2 = j;
+                    if (pickedcards[match1].Text == pickedcards[match2].Text)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+
+        }
+        private void DoComputerMove()
+        {
+            //if set was already uncovered then pick it
+            if (PickedCardsMatch())
+            {
+                PlayCard(pickedcards[match1]);
+                TwoSecDelay();
+                if (gamestatus == GameStatusEnum.playing)
+                {
+                    PlayCard(pickedcards[match2]);
+                }
+            }
+
+            //if set was not picked pick rnd card
+            else
+            {
+                availablecards.Clear();
+                availablecards = allcards.Where(c => c.Visible == true).ToList();
+                PlayCard(availablecards[rnd.Next(availablecards.Count)]);
+            }
+            //check if matches if not pick another rnd card
+
+        }
         private void ShuffleCards()
         {
             sets.Clear();
@@ -126,7 +204,7 @@
                 }
                 sets.Add(new() { remaingcards[card_one], remaingcards[card_two] });
 
-                //The following removes the cards that already have a picture
+                //The following removes the cards that are already in the set list
                 remaingcards.RemoveAt(card_one);
                 //To make sure it doesn't attempt to remove an index that is out of bound because card 1 was already removed
                 if (card_one < card_two)
@@ -137,7 +215,6 @@
             }
 
             //Will add different picture to each set
-            //??Need help to figure out why the char keeps on moving forward and doesn't reuse, then change back to windings font
             sets.ForEach(s =>
                   {
                       s.ForEach(c => c.Text = picture.ToString());
@@ -152,7 +229,14 @@
 
             if (gamestatus == GameStatusEnum.playing)
             {
-                msg = currentturn == TurnEnum.player1 ? "Player 1's Turn" : "Player 2's Turn";
+                if (optSolo.Checked)
+                {
+                    msg = currentturn == TurnEnum.player1 ? "Player 1's Turn" : "Computer's Turn";
+                }
+                else
+                {
+                    msg = currentturn == TurnEnum.player1 ? "Player 1's Turn" : "Player 2's Turn";
+                }
                 btnmessage = "New Game";
                 c = Color.Green;
             }
@@ -168,6 +252,8 @@
                 }
                 c = Color.MediumVioletRed;
             }
+
+
             lblMessage.ForeColor = c;
             lblMessage.Text = msg;
             btnStart.Text = btnmessage;
@@ -177,18 +263,23 @@
             optSolo.Enabled = gamestatus != GameStatusEnum.playing ? true : false;
         }
 
-        private void Card_Click(object? sender, EventArgs e)
-        {
-            if (sender is Button btn)
-            {
-                ExposeCard(btn);
-            }
-        }
+        //Event handlers
         private void BtnStart_Click(object? sender, EventArgs e)
         {
             StartNewGame();
         }
+        private void Card_Click(object? sender, EventArgs e)
+        {
+            //Add in to the if statement  && lblMessage.Text != "Computer's Turn"
+            if (sender is Button btn)
+            {
+                PlayCard(btn);
+            }
+        }
+        private void Opt_CheckedChanged(object? sender, EventArgs e)
+        {
+            lblPlayerMode.Text = optSolo.Checked ? "Solo" : "2 Player";
+        }
     }
 }
 
-//For computer turn make a list that will keep tab of all cards that where already picked

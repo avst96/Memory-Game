57ec2d781d9b7945128680553c2f28a47c51e23b
avst96 <avst96@gmail.com>
2023-12-12 15:13:12 -0600

    Started Object Game and Card

---
+++ MemoryGameApp/MemoryGameSystem/Card.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+
+namespace MemoryGameSystem
+{
+    public class Card : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        public char CardPicture {  get; internal set; }
+
+        public Color CardColors { get; private set; }
+
+        //add for forecolor??
+        public void ShowCard()
+        {
+
+        }
+        public void HideCard()
+        {
+
+        }
+
+        public void Clear()
+        {
+           
+        }
+
+
+
+
+        private void InvokePropertyChanged([CallerMemberName] string propertyname = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+        }
+    }
+}
---
+++ MemoryGameApp/MemoryGameSystem/MemoryGame.cs
@@ -0,0 +1,14 @@
+﻿namespace MemoryGameSystem
+{
+    internal class MemoryGame
+    {
+        List<Card> allcards = new();
+        public MemoryGame() 
+        { 
+            for(int i = 0; i < 20; i++)
+            {
+                allcards.Add(new Card());
+            }
+        }
+    }
+}

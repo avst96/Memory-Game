c3611fda06367620c49d61eead078c9dafb661e3
avst96 <avst96@gmail.com>
2023-12-12 17:13:12 -0600

    Cards in process

--- MemoryGameApp/MemoryGameSystem/Card.cs
+++ MemoryGameApp/MemoryGameSystem/Card.cs
@@ -7,26 +7,25 @@ namespace MemoryGameSystem
     public class Card : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
+        public enum CardStatusEnum { Facedown, Faceup, Hidden};
+        internal CardStatusEnum CardStatus { get; set; }
+       //public char CardPicture {  get; internal set; }
 
+        public System.Drawing.Color BackColor { get => Color.FromArgb(CardStatus == CardStatusEnum.Hidden ? 255 : 0, 255, 165, 0); }
+        public System.Drawing.Color ForeColor { get => CardStatus == CardStatusEnum.Faceup ? Color.Black : BackColor; }
+        //public void ShowCard()
+        //{
 
-        public char CardPicture {  get; internal set; }
+        //}
+        //public void HideCard()
+        //{
 
-        public Color CardColors { get; private set; }
+        //}
 
-        //add for forecolor??
-        public void ShowCard()
-        {
-
-        }
-        public void HideCard()
-        {
-
-        }
-
-        public void Clear()
-        {
+        //public void Clear()
+        //{
            
-        }
+        //}
 
 
 

ecb4dbe31bd8ae5b91743f52ef79f745632df493
avst96 <avst96@gmail.com>
2024-02-27 19:33:57 -0600

    HW P3 #11 In done

---
+++ MemoryGameApp/MemoryGameAsp.net/Pages/Game - jQuery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MemoryGameAsp.net.Pages
+{
+    public class Game____jQueryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

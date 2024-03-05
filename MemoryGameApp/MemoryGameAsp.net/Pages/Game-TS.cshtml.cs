62109bf4fc5bfeec37773b64d0de86c683df81ee
avst96 <avst96@gmail.com>
2024-03-04 19:03:36 -0600

    Setup for HW 3.13 TS

---
+++ MemoryGameApp/MemoryGameAsp.net/Pages/Game-TS.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MemoryGameAsp.net.Pages
+{
+    public class Game_TSModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
